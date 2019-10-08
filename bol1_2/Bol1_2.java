f8c1e8db45afcae1cbd136e01f78ed86f2db6a76
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-08 08:49:50 +0200

    primer commit

---
+++ bol1_2/Bol1_2.java
@@ -0,0 +1,9 @@
+package bol1_2;
+public class Bol1_2 {
+    public static void main(String[] args) {
+         int l = 3, Area;
+        Area = l*l;
+        System.out.println("Area del cuadrado de lado=3 es="+Area );
+    }
+    
+}
